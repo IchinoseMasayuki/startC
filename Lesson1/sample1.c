5813227a41ceff78729cbbac90b190e8f0093aea
IchinoseMasayuki <noseichi29@gmail.com>
2016-05-24 19:07:22 +0900

    Add files via upload

--- Lesson1/sample1.c
+++ Lesson1/sample1.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("ようこそC言語へ\n");
+#include <stdio.h>			/*stdio.hというファイルを#で最優先に読み込む*/
+					/*stdio.h等をヘッダーファイル(header file)という*/
+int main(void)				/*Integer(整数)	4bitの整数を格納することができる。*/
+{					/*main(主要部)	ここから本体である。*/
+					/*void		ここではmain関数に置ける変数がないこと。*/
+	printf("ようこそC言語へ\n");	/*printf	()内の""内を表示する。*/
+					/*\n		改行*/
 	
-	return 0;
+	return 0;			/*return	値を返す。*/
 }
